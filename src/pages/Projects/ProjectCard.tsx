9774d3df18be116cfcd57c8bc39d990c3e8998bb
htldacosta <hitaludcosta@gmail.com>
2025-11-28 17:06:21 -0400

    refactoring

---
+++ src/pages/Projects/ProjectCard.tsx
@@ -0,0 +1,60 @@
+import { Project } from "../../types/Project";
+import { FaGithub, FaFileAlt, FaCode } from "react-icons/fa";
+import styles from "./Projects.module.css";
+
+interface ProjectCardProps {
+  data: Project;
+}
+
+export const ProjectCard = ({ data }: ProjectCardProps) => {
+  return (
+    <article className={styles.portfolioCard}>
+      <div className={styles.cardImagePlaceholder}>
+        {data.image ? (
+          <img
+            src={data.image}
+            alt={data.title}
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+        ) : (
+          <FaCode />
+        )}
+      </div>
+
+      <div className={styles.cardContent}>
+        <h3>{data.title}</h3>
+        <p>{data.description}</p>
+
+        <div className={styles.cardTech}>
+          {data.technologies.map((tech) => (
+            <span key={tech} className={styles.techTag}>
+              {tech}
+            </span>
+          ))}
+        </div>
+
+        <div className={styles.cardButtons}>
+          {data.docLink && (
+            <a
+              href={data.docLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`${styles.btnCard} ${styles.btnPrimary}`}
+            >
+              <FaFileAlt style={{ marginRight: "5px" }} /> Docs
+            </a>
+          )}
+
+          <a
+            href={data.repoLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${styles.btnCard} ${styles.btnSecondary}`}
+          >
+            <FaGithub style={{ marginRight: "5px" }} /> Repo
+          </a>
+        </div>
+      </div>
+    </article>
+  );
+};
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,61 +1,55 @@
 import { Project } from "../../types/Project";
-import { FaGithub, FaFileAlt, FaCode } from "react-icons/fa";
+import { ProjectCard } from "./ProjectCard";
 import styles from "./Projects.module.css";
 
-interface ProjectCardProps {
-  data: Project;
-}
+export const Projects = () => {
+  const projectsList: Project[] = [
+    {
+      id: 1,
+      title: "Serviço de E-mail",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Um serviço robusto para envio de emails transacionais.",
+      technologies: ["Java", "Spring Boot", "AWS"],
+      repoLink: "https://github.com/htldacosta",
+      docLink: "https://github.com/htldacosta",
+    },
+    {
+      id: 2,
+      title: "Newsletter System",
+      description:
+        "Sistema completo de newsletter com agendamento e gestão de assinantes.",
+      technologies: ["Java", "Spring", "MongoDB", "AWS"],
+      repoLink: "https://github.com/htldacosta",
+      docLink: "#",
+    },
+    {
+      id: 3,
+      title: "Cadastro de Alunos",
+      description:
+        "CRUD completo para gestão acadêmica com relatórios e dashboard.",
+      technologies: ["Java", "PostgreSQL", "Spring"],
+      repoLink: "https://github.com/htldacosta",
+      docLink: "#",
+    },
+    {
+      id: 4,
+      title: "Simulador de Investimentos",
+      description:
+        "Aplicação fullstack para simulação de rendimentos com base em índices financeiros.",
+      technologies: ["Java", "Spring Boot", "React.js", "Golang", "PostgreSQL"],
+      repoLink: "https://github.com/htldacosta",
+    },
+  ];
 
-export const ProjectCard = ({ data }: ProjectCardProps) => {
   return (
-    <article className={styles.portfolioCard}>
-      {/* Se tiver imagem, mostra ela. Se não, mostra um placeholder com ícone */}
-      <div className={styles.cardImagePlaceholder}>
-        {data.image ? (
-          <img
-            src={data.image}
-            alt={data.title}
-            style={{ width: "100%", height: "100%", objectFit: "cover" }}
-          />
-        ) : (
-          <FaCode />
-        )}
+    <section className={styles.portfolioSection}>
+      <h2 className={styles.sectionTitle}>Meus Projetos</h2>
+
+      <div className={styles.portfolioGrid}>
+        {projectsList.map((project) => (
+          <ProjectCard key={project.id} data={project} />
+        ))}
       </div>
-
-      <div className={styles.cardContent}>
-        <h3>{data.title}</h3>
-        <p>{data.description}</p>
-
-        <div className={styles.cardTech}>
-          {data.technologies.map((tech) => (
-            <span key={tech} className={styles.techTag}>
-              {tech}
-            </span>
-          ))}
-        </div>
-
-        <div className={styles.cardButtons}>
-          {data.docLink && (
-            <a
-              href={data.docLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={`${styles.btnCard} ${styles.btnPrimary}`}
-            >
-              <FaFileAlt style={{ marginRight: "5px" }} /> Docs
-            </a>
-          )}
-
-          <a
-            href={data.repoLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className={`${styles.btnCard} ${styles.btnSecondary}`}
-          >
-            <FaGithub style={{ marginRight: "5px" }} /> Repo
-          </a>
-        </div>
-      </div>
-    </article>
+    </section>
   );
 };
